82450b8c16206d8ffcc486f9dd3bb07620b56f26
Abdulmajid Usman <abstractmajid@gmail.com>
2022-08-02 21:50:39 +0100

    add login screen

--- lib/main.dart
+++ lib/main.dart
@@ -2,6 +2,7 @@ import 'package:flutter/material.dart';
 import 'package:mobile_assessment_flutter/core/routes/app_route.dart';
 import 'package:mobile_assessment_flutter/core/theme/shipify_theme.dart';
 import 'package:mobile_assessment_flutter/features/registration/screens/choose_user_type_screen.dart';
+import 'package:mobile_assessment_flutter/features/registration/screens/login_screen.dart';
 import 'package:mobile_assessment_flutter/features/registration/screens/sign_up_screen.dart';
 
 void main() {
@@ -19,7 +20,7 @@ class ShipifyApp extends StatelessWidget {
       theme: ShipifyTheme.theme,
       navigatorKey: AppRoute.navigatorKey,
       onGenerateRoute: AppRoute.generateRoute,
-      home: const SignUpScreen(),
+      home: const LoginScreen(),
     );
   }
 }
