0fc567abfd11929e8dd31a9f2ec2431bf5d1a8cd
Tunmise-Adegoke <mhizsubomi@gmail.com>
2022-08-02 15:10:58 +0100

    splash screen updated

---
+++ lib/screens/splash_screens.dart
@@ -0,0 +1,28 @@
+import 'package:animated_splash_screen/animated_splash_screen.dart';
+import 'package:flutter/material.dart';
+
+class SplashScreen extends StatelessWidget {
+  const SplashScreen({Key? key}) : super(key: key);
+
+  @override
+  Widget build(BuildContext context) {
+    return AnimatedSplashScreen(
+        backgroundColor: Colors.black,
+        splash: Image.asset(
+          'assets/images/splash_logo.png',
+          fit: BoxFit.contain,
+        ),
+        duration: 20,
+        splashTransition: SplashTransition.scaleTransition,
+        nextScreen: Home());
+  }
+}
+
+class Home extends StatelessWidget {
+  const Home({Key? key}) : super(key: key);
+
+  @override
+  Widget build(BuildContext context) {
+    return Container();
+  }
+}
