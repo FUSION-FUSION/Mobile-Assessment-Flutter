10dfdd37d2f31dde4c3d3644bacc453d52dbdcb8
Tunmise-Adegoke <mhizsubomi@gmail.com>
2022-08-02 16:34:32 +0100

    changed colors and added fonts

--- lib/utils/constant.dart
+++ lib/utils/constant.dart
@@ -1,7 +1,7 @@
 import 'package:flutter/material.dart';
 
-const kPrimaryBlue = Color.fromARGB(255, 112, 199, 239);
+const kPrimaryBlue = Color(0xff46A5BA);
 const kPrimaryWhite = Colors.white;
 const kPrimaryblueGrey = Color.fromARGB(255, 205, 216, 222);
 const kPrimaryBlack = Colors.black;
-const kPrimaryRed = Colors.red;
+const kPrimaryRed = Color(0xffFF0000);
