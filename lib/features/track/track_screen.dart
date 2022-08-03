446e45fc033b011e88ea13e8bbd22f7e4eef55a4
Victor Adepoju <victoradepoju30@gmail.com>
2022-08-04 00:00:50 +0100

    installed modal_bottom_sheet package

--- lib/features/track/track_screen.dart
+++ lib/features/track/track_screen.dart
@@ -1,8 +1,14 @@
 import 'package:flutter/material.dart';
+import 'package:solid_bottom_sheet/solid_bottom_sheet.dart';
 
-class TrackScreen extends StatelessWidget {
+class TrackScreen extends StatefulWidget {
   const TrackScreen({Key? key}) : super(key: key);
 
+  @override
+  State<TrackScreen> createState() => _TrackScreenState();
+}
+
+class _TrackScreenState extends State<TrackScreen> {
   @override
   Widget build(BuildContext context) {
     return Scaffold(
