98fb3fed14df2234fe55c69c27012f46f8bb2432
Samson Moses <knomic10@gmail.com>
2022-08-04 00:22:43 +0100

    added screenshots for all pages so far

--- lib/main.dart
+++ lib/main.dart
@@ -1,6 +1,6 @@
 import 'package:flutter/material.dart';
 import 'package:get/get.dart';
-import 'package:mobile_assessment_flutter/screens/splash/splash_screen.dart';
+import 'package:mobile_assessment_flutter/screens/auth/login.dart';
 
 void main() {
   runApp(const MyApp());
@@ -27,7 +27,7 @@ class MyApp extends StatelessWidget {
         // is not restarted.
         primarySwatch: Colors.blue,
       ),
-      home: const SplashScreen(),
+      home: const Login(),
     );
   }
 }
