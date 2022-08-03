890909cdb676f34ad8e92b805d5b0e6d4e0dc0f2
Victor Adepoju <victoradepoju30@gmail.com>
2022-08-03 02:43:45 +0100

    added animated splash screen

--- lib/main.dart
+++ lib/main.dart
@@ -1,5 +1,7 @@
+import 'package:animated_splash_screen/animated_splash_screen.dart';
 import 'package:flutter/material.dart';
 import 'package:mobile_assessment_flutter/features/routing/app_router.dart';
+import 'package:page_transition/page_transition.dart';
 
 void main() {
   runApp(const MyApp());
@@ -10,11 +12,30 @@ class MyApp extends StatelessWidget {
 
   @override
   Widget build(BuildContext context) {
-    return MaterialApp.router(
-      routerDelegate: goRouter.routerDelegate,
-      routeInformationParser: goRouter.routeInformationParser,
+    return const MaterialApp(
       debugShowCheckedModeBanner: false,
-      restorationScopeId: 'app',
+      home: SplashScreen(),
+    );
+  }
+}
+
+class SplashScreen extends StatelessWidget {
+  const SplashScreen({Key? key}) : super(key: key);
+
+  @override
+  Widget build(BuildContext context) {
+    return AnimatedSplashScreen(
+      splash: Image.asset('assets/splash_logo.png'),
+      nextScreen: MaterialApp.router(
+        routerDelegate: goRouter.routerDelegate,
+        routeInformationParser: goRouter.routeInformationParser,
+        debugShowCheckedModeBanner: false,
+        restorationScopeId: 'app',
+      ),
+      backgroundColor: Colors.black,
+      duration: 3000,
+      splashTransition: SplashTransition.scaleTransition,
+      pageTransitionType: PageTransitionType.rightToLeft,
     );
   }
 }
