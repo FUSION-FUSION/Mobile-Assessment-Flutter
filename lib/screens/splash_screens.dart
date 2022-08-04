534a9c52a02542b039be4ac6166b67d5c226a862
Tunmise-Adegoke <mhizsubomi@gmail.com>
2022-08-04 04:18:15 +0100

    merging branches

--- lib/screens/splash_screens.dart
+++ lib/screens/splash_screens.dart
@@ -19,11 +19,3 @@ class SplashScreen extends StatelessWidget {
   }
 }
 
-class Home extends StatelessWidget {
-  const Home({Key? key}) : super(key: key);
-
-  @override
-  Widget build(BuildContext context) {
-    return Container();
-  }
-}
