820ee661c9b40d0697ade713a1206e3726ad8f6c
Samson Moses <knomic10@gmail.com>
2022-08-05 12:06:04 +0100

    added routes back

--- lib/main.dart
+++ lib/main.dart
@@ -1,7 +1,6 @@
 import 'package:flutter/material.dart';
 import 'package:get/get.dart';
 import 'package:mobile_assessment_flutter/routes/route_helper.dart';
-import 'package:mobile_assessment_flutter/waybill/waybill.dart';
 
 void main() {
   runApp(const MyApp());
@@ -17,20 +16,10 @@ class MyApp extends StatelessWidget {
       debugShowCheckedModeBanner: false,
       title: 'Flutter Demo',
       theme: ThemeData(
-        // This is the theme of your application.
-        //
-        // Try running your application with "flutter run". You'll see the
-        // application has a blue toolbar. Then, without quitting the app, try
-        // changing the primarySwatch below to Colors.green and then invoke
-        // "hot reload" (press "r" in the console where you ran "flutter run",
-        // or simply save your changes to "hot reload" in a Flutter IDE).
-        // Notice that the counter didn't reset back to zero; the application
-        // is not restarted.
         primarySwatch: Colors.blue,
       ),
-      // initialRoute: RouteHelper.initial,
-      // getPages: RouteHelper.routes,
-      home: const WaybillPage(),
+      initialRoute: RouteHelper.initial,
+      getPages: RouteHelper.routes,
     );
   }
 }
